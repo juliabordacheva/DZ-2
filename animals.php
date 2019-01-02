55ed63e64e613ec32540c803818049b24fe94bf3
Юлия Бордачева <s_proza@list.ru>
2019-01-02 21:45:36 +0300

    2c

--- animals.php
+++ animals.php
@@ -28,48 +28,43 @@ $animals = ['Europe' => ['Cervus elaphus', 'Erinaceus europaeus'],
 $animals2 = []; 
 foreach($animals as $areaName => $animalArray) {
     foreach ($animalArray as $animalName) {
-        if (strpos($animalName, ' ') !== false) {
+        if (substr_count($animalName, ' ') == 1) {
           $animals2[] = $animalName;
         }
     }
 }
-print_r($animals2); 
+ 
 
 $b = implode(' ', $animals2);
 
-var_dump($b); 
+
  
  $c = explode(' ', $b);
 
- var_dump($c);
+ 
 
  
 
  foreach ($c as $key => $value) {
         if (($key + 1) % 2 != 0) {
             $even[] = $value;
-            print_r($even) . '<br />';
-            echo '<pre>';
+           
+        } else {
+         if (($key + 1) % 2 == 0) {
+            $odd[] = $value; 
         }
     }
-
- foreach ($c as $key => $value) {
-        if (($key + 1) % 2 == 0) {
-            $odd[] = $value;
-            print_r($odd) . '<br />';
-            echo '<pre>';
-        }
-    }
+}
 
     shuffle($even);
-    print_r($even);
+    
     
 $result1 = array_combine($even, $odd);
-print_r($result1);
+
 
 foreach ($result1 as $key => $value) {
   $result2 =  $key  . ' ' .  $value . '<br>';
-  print_r($result2);
+  print($result2);
 }
 
 
