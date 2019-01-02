e572dea8bc78394d15a0a6629fec236026fbe5b0
Юлия Бордачева <s_proza@list.ru>
2019-01-02 23:51:04 +0300

    3c

--- animals.php
+++ animals.php
@@ -1,26 +1,12 @@
-<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>Животные</title>
-    </head>
-    <body>
-      <h1>Животные</h1>
-
-      
-      
-    </body>
-  </html> 
-
   <?php 
 $animals = ['Europe' => ['Cervus elaphus', 'Erinaceus europaeus'], 
-	             'Asia' => ['Ailuropoda melanoleuca', 'Pholidota'],
-	             'Africa' => ['Giraffa camelopardalis',  'Troglodytes gorilla'],
-	             'Australia' => ['Macropus rufus',  'Vombatidae'],
-	             'India' => ['Elephas maximus'], 
-	             'Antarctida' => ['Aptenodytes forsteri', 'Leptonychotes weddellii'],
-	             'North America' => ['Vulpes lagopus', 'Ovibos'],
-	             'Southern America' => ['Cacajao calvus', 'Vicugna pacos', 'Lagidium']];
+               'Asia' => ['Ailuropoda melanoleuca', 'Pholidota'],
+               'Africa' => ['Giraffa camelopardalis',  'Troglodytes gorilla'],
+               'Australia' => ['Macropus rufus',  'Vombatidae'],
+               'India' => ['Elephas maximus'], 
+               'Antarctida' => ['Aptenodytes forsteri', 'Leptonychotes weddellii'],
+               'North America' => ['Vulpes lagopus', 'Ovibos'],
+               'Southern America' => ['Cacajao calvus', 'Vicugna pacos', 'Lagidium']];
 
 
 
@@ -63,9 +49,42 @@ $result1 = array_combine($even, $odd);
 
 
 foreach ($result1 as $key => $value) {
-  $result2 =  $key  . ' ' .  $value . '<br>';
-  print($result2);
-}
+  $result2 =  $key  . ' ' .  $value .'<br>';
+ $fant[] = $result2;
+ }
 
 
 ?>
+
+
+
+
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Животные</title>
+    </head>
+    <body>
+      <h1>Животные</h1>
+
+      <p><?=  $fant[0]; ?></p>
+      <p><?=  $fant[1]; ?></p> 
+      <p><?=  $fant[2]; ?></p>
+      <p><?=  $fant[3]; ?></p>
+      <p><?=  $fant[4]; ?></p>
+      <p><?=  $fant[5]; ?></p>
+      <p><?=  $fant[6]; ?></p>
+      <p><?=  $fant[7]; ?></p>
+      <p><?=  $fant[8]; ?></p>
+      <p><?=  $fant[9]; ?></p>
+      <p><?=  $fant[10]; ?></p>
+      <p><?=  $fant[11]; ?></p>
+      
+
+      
+      
+      
+    </body>
+  </html> 
+
